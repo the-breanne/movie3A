f824c46582ee4e440b6e4fca914ac33112a7353e
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-09-25 15:07:48 -0500

    Update settings.py

--- movie/settings.py
+++ movie/settings.py
@@ -174,3 +174,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
